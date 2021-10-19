b38ed5f9d9fe6b1209d2f34cc0e11dfddce8759e
ArsalanJamali <jamaliarsalan@yahoo.com>
2021-10-20 00:50:14 +0500

    layout changed

--- InvoiceParser/settings.py
+++ InvoiceParser/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static'),
+# ]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 #Tessesract Location
 #tesseract_location="C:\\Program Files\\Tesseract-OCR\\tesseract.exe" 
@@ -133,4 +135,5 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 
 LOGIN_URL='/admin/'
+
 
